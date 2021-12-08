335452eba079b63f017c6d9b2900852fe039589f
Zahin <alvii147@gmail.com>
2021-12-08 21:43:50 +0600

    fixed order of load command

--- src/load.sql
+++ src/load.sql
@@ -5,13 +5,13 @@ tee output_load.txt
 
 LOAD DATA INFILE '/var/lib/mysql-files/10-Crime/UKCrime/london-stop-and-search.csv'
 INTO TABLE Location
-(@dummy, @dummy, @dummy, @dummy, latitude, longitude)
 FIELDS
     TERMINATED BY ','
     ENCLOSED BY '"'
 LINES
     TERMINATED BY '\r\n'
 IGNORE 1 LINES
+(@dummy, @dummy, @dummy, @dummy, latitude, longitude)
 SET country='United Kingdom';
 
 notee
