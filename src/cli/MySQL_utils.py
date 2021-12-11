8e4bc35cc034a187b6130027cac41308fa500b46
Zahin <alvii147@gmail.com>
2021-12-12 03:06:09 +0600

    added warning for config password; renamed functions

--- src/cli/MySQL_utils.py
+++ src/cli/MySQL_utils.py
@@ -1,10 +1,29 @@
 from configparser import ConfigParser
 from getpass import getpass
+import warnings
+
 from mysql.connector import connect, Error
 
-DB_CONFIG_SECTION = 'mysqlconfig'
+def DBConfig(config_file_path='config.ini', config_section='mysqlconfig'):
+    '''
+    Parse config file and get user input for database configuration
+    variables.
 
-def DBConfig(config_file_path='config.ini'):
+    Parameters
+    ----------
+    config_file_path : str
+        Absolute or relative path to config file.
+    config_section : str
+        Section name in config file that contains database configuration.
+
+    Returns
+    -------
+    config_vars : dict
+        Dictionary containing the host name, username, password and
+        database name.
+    '''
+
+    # parse config file
     config = ConfigParser()
     config.read(config_file_path)
 
@@ -18,27 +37,57 @@ def DBConfig(config_file_path='config.ini'):
     config_vars = {}
 
     for var_name in config_var_names:
-        var_value = config.get(DB_CONFIG_SECTION, var_name, fallback=None)
+        # get config variables from config file
+        var_value = config.get(config_section, var_name, fallback=None)
 
-        if var_value is not None:
-            config_vars[var_name] = var_value
-        else:
-            if var_name != 'password':
-                config_vars[var_name] = input(f'{var_name}: ')
-            else:
+        if var_value is None:
+            # if config variable not found, prompt for it using stdin
+            if var_name == 'password':
+                # if password config variable, get stdin without echo
                 config_vars[var_name] = getpass(f'{var_name}: ')
+            else:
+                config_vars[var_name] = input(f'{var_name}: ')
+        else:
+            # if password config variable found, throw warning
+            if var_name == 'password':
+                warnings.warn(
+                    'Storing plain text format password in config file ' \
+                    'is not recommended.'
+                )
+
+            config_vars[var_name] = var_value
 
     return config_vars
 
-def DQL(queries):
-    results = []
-    try:
-        with connect(**DBConfig()) as connection:
-            with connection.cursor() as cursor:
-                for query in queries:
-                    cursor.execute(query)
-                    results.append(cursor.fetchall())
-    except Error as e:
-        print(e)
+def RunQuery(queries, commit=False):
+    '''
+    Run MySQL query.
 
-    return results
+    Parameters
+    ----------
+    queries : list
+        List of queries to run.
+    commit : bool
+        Whether or not to commit transaction. This should be set to true
+        for DDL, DML, DCL and TCL queries, and false for DQL queries.
+
+    Returns
+    -------
+    output : list
+        List of output tables of each query.
+    '''
+
+    output = []
+    # connect to database
+    with connect(**DBConfig()) as connection:
+        with connection.cursor() as cursor:
+            for query in queries:
+                # run each query and store output
+                cursor.execute(query)
+                output.append(cursor.fetchall())
+
+                # commit if needed
+                if commit:
+                    connection.commit()
+
+    return output
