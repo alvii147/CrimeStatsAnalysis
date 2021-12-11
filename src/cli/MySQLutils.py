b6a1244245f96d9a43a8066abd3fdc1d9c01c1ac
Zahin <alvii147@gmail.com>
2021-12-11 19:01:08 +0600

    initial commit for python based cli

---
+++ src/cli/MySQLutils.py
@@ -0,0 +1,46 @@
+from configparser import ConfigParser
+from getpass import getpass
+from mysql.connector import connect, Error
+
+DB_CONFIG_SECTION = 'mysqlconfig'
+
+def getDatabaseConfig(config_file_path='config.ini'):
+    config = ConfigParser()
+    config.read(config_file_path)
+
+    config_var_names = [
+        'host',
+        'user',
+        'password',
+        'database',
+    ]
+
+    config_vars = {}
+
+    for var_name in config_var_names:
+        var_value = config.get(DB_CONFIG_SECTION, var_name, fallback=None)
+
+        if var_value is not None:
+            config_vars[var_name] = var_value
+        else:
+            if var_name != 'password':
+                config_vars[var_name] = input(f'{var_name}: ')
+            else:
+                config_vars[var_name] = getpass(f'{var_name}: ')
+
+    return config_vars
+
+def MySQL_DQL(queries):
+    DBCONFIG = getDatabaseConfig()
+
+    results = []
+    try:
+        with connect(**DBCONFIG) as connection:
+            with connection.cursor() as cursor:
+                for query in queries:
+                    cursor.execute(query)
+                    results.append(cursor.fetchall())
+    except Error as e:
+        print(e)
+
+    return results
