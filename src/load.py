e8978e584a7f27ff600f47336aa3b9ef75ea65cb
Zahin <alvii147@gmail.com>
2021-12-13 21:18:54 +0600

    fixed quotes issue again

--- src/load.py
+++ src/load.py
@@ -18,21 +18,21 @@ if __name__ == '__main__':
 
         query = 'INSERT INTO Incident '
         query += '(location_id, occurrence_date, type) '
-        query += f'VALUES ({location_id}, {row[1]}, {row[0]});'
+        query += f'VALUES ({location_id}, "{row[1]}", "{row[0]}");'
 
         cursor.execute(query)
         incident_id = cursor.lastrowid
 
         query = 'INSERT INTO Person '
         query += '(age_range, gender, ethnicity) '
-        query += f'VALUES ({row[4]}, {row[5]}, {row[6]});'
+        query += f'VALUES ("{row[4]}", "{row[5]}", "{row[6]}");'
 
         cursor.execute(query)
         suspect_id = cursor.lastrowid
 
         query = 'INSERT INTO Search '
         query += '(incident_id, suspect_id, legislation, object, outcome, object_caused_outcome, clothing_removal) '
-        query += f'VALUES ({incident_id}, {suspect_id}, {row[7]}, {row[8]}, {row[9]}, {row[10]}, {row[11]})'
+        query += f'VALUES ({incident_id}, {suspect_id}, "{row[7]}", "{row[8]}", "{row[9]}", "{row[10]}", "{row[11]})"'
 
         cursor.execute(query)
 
