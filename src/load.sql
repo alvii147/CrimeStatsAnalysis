1ca32596c8cff74f856dffbbde1b7e415b7c2f5f
Zahin <alvii147@gmail.com>
2021-12-13 07:53:09 +0600

    fixed error, changed to load on 100 entries

--- src/load.sql
+++ src/load.sql
@@ -21,7 +21,7 @@ FIELDS
     ENCLOSED BY '"'
 LINES
     TERMINATED BY '\n'
-IGNORE 1 LINES
+IGNORE 302524 LINES
 (type, @_occurrence_date, @_dummy, @_dummy, @_latitude, @_longitude, gender, age_range, @_dummy, ethnicity, legislation, object, outcome, @_object_caused_outcome, @_clothing_removal)
 SET
     occurrence_date = CAST(@_occurrence_date AS DATE),
@@ -39,7 +39,7 @@ FIELDS
     ENCLOSED BY '"'
 LINES
     TERMINATED BY '\n'
-IGNORE 1 LINES
+IGNORE 1946951 LINES
 (@_dummy, @_occurrence_date, precinct, @_dummy, @_longitude, @_latitude, @_dummy, lsoa_code, @_dummy, description)
 SET
     occurrence_date = CAST(CONCAT(@_occurrence_date, '-01') AS DATE),
@@ -55,7 +55,7 @@ FIELDS
     ENCLOSED BY '"'
 LINES
     TERMINATED BY '\n'
-IGNORE 1 LINES
+IGNORE 2946580 LINES
 (@_dummy, @_occurrence_date, precinct, @_dummy, @_longitude, @_latitude, @_dummy, lsoa_code, @_dummy, type, description, @_dummy)
 SET
     occurrence_date = CAST(CONCAT(@_occurrence_date, '-01') AS DATE),
@@ -71,11 +71,11 @@ FIELDS
     ENCLOSED BY '"'
 LINES
     TERMINATED BY '\n'
-IGNORE 1 LINES
-(@_dummy, @_occurrence_date, @_dummy, @_dummy, @_dummy, @_reported_date, code, @_dummy, @_dummy, description, @_dummy, type, @_dummy, @_dummy, borough, precinct, , @_dummy, @_dummy, @_dummy, @_dummy, @_dummy, @_dummy, @_latitude, @_longitude)
+IGNORE 1048477 LINES
+(@_dummy, @_occurrence_date, @_dummy, @_dummy, @_dummy, @_reported_date, code, @_dummy, @_dummy, description, @_dummy, type, @_dummy, @_dummy, borough, precinct, @_dummy, @_dummy, @_dummy, @_dummy, @_dummy, @_dummy, @_latitude, @_longitude)
 SET
-    occurrence_date = CAST(CONCAT(@_occurrence_date, '-01') AS DATE),
-    reported_date = CAST(CONCAT(@_reported_date, '-01') AS DATE),
+    occurrence_date = CAST(@_occurrence_date AS DATE),
+    reported_date = CAST(@_reported_date AS DATE),
     latitude = NULLIF(@_latitude, ''),
     longitude = NULLIF(@_longitude, '');
 
