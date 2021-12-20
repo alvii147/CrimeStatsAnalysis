f283ea9a29d681569f66a53bce6adf0ea0a31162
Kyle Pinto <kylepinto@yahoo.ca>
2021-12-19 21:14:56 -0500

    FK for specializations

--- src/SQL/keys.sql
+++ src/SQL/keys.sql
@@ -1,3 +1,12 @@
+ALTER TABLE Complaint ADD CONSTRAINT Complaint_Incident
+    FOREIGN KEY (incident_id) REFERENCES Incident(incident_id);
+
+ALTER TABLE Crime ADD CONSTRAINT Crime_Incident
+    FOREIGN KEY (incident_id) REFERENCES Incident(incident_id);
+
+ALTER TABLE Search ADD CONSTRAINT Search_Incident
+    FOREIGN KEY (incident_id) REFERENCES Incident(incident_id);
+
 ALTER TABLE Incident ADD CONSTRAINT happensIn
     FOREIGN KEY (location_id) REFERENCES Location(location_id);
 
