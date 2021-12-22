a1803b5c4583740f52e03afb41edbf13c5dff2a6
Zahin <alvii147@gmail.com>
2021-12-22 22:11:26 +0600

    added debug statement

--- src/db.py
+++ src/db.py
@@ -162,6 +162,8 @@ def getIgnoreLines(n, path, safety_net=10):
     elif ignore_lines > entries - safety_net:
         ignore_lines = entries - safety_net
 
+    log.info(f'Ignoring {ignore_lines} lines from {path}')
+
     return ignore_lines
 
 def loadReplaceIgnoreLines(query, n):
